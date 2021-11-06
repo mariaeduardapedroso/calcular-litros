2ac09a53d3bb25ed12e5a2919f8b478bf55dbd2b
Maria Eduarda Pedroso <93790097+mariaeduardapedroso@users.noreply.github.com>
2021-11-06 20:42:59 -0300

    Update script.js

--- script.js
+++ script.js
@@ -4,12 +4,12 @@ function calcularlitros() {
   var rendimento = parseFloat(txtRendimento.value);
 
   if (isNaN(velocidade) || isNaN(tempo) || isNaN(rendimento)) {
-    txtResultado.innerHTML = "Erro Valor inválido";
+    txtResultado.innerHTML = "Erro No Valor";
   } else {
     var distancia = (tempo / 60) * velocidade;
     var litrosconsumidos = distancia / rendimento;
 
     txtResultado.innerHTML =
-      "Foram gastros " + litrosconsumidos.toFixed(2) + " litros";
+      "Foram gastos " + litrosconsumidos.toFixed(2) + " litros";
   }
 }
